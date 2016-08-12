55ea703d892329762af98e18a266b055a224080a
altrane1149 <dont_forget_alamo@yahoo.co.jp>
2016-08-13 02:32:00 +0900

    ObjectFieldTest

---
+++ Fighting/Assets/Scripts/Editor/ObjectFieldTest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class ObjectFieldTest : EditorWindow
+{
+    public Texture texture;
+
+    [MenuItem("Window/ObjectFieldTest")]
+    static void Open()
+    {
+        EditorWindow.GetWindow<ObjectFieldTest>("ObjectFieldTest"); // ウィンドウのタイトル
+    }
+
+	void OnGUI()
+    {
+        texture = EditorGUILayout.ObjectField(texture, typeof(Texture), true) as Texture;
+    }
+}
