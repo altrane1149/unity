ca4c990c223e067d07886dac60e0617b894d035f
altrane1149 <dont_forget_alamo@yahoo.co.jp>
2016-08-18 03:13:27 +0900

    Script追加

---
+++ Fighting/Assets/Scripts/BallManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections;
+
+public class BallManager : MonoBehaviour {
+
+    public Camera mainCamera;
+    public GameObject ballPrefab;
+    public Vector3 force = new Vector3(0, 0, 1000);
+
+    private GameObject ballObjTmp;
+    private Vector3 ballPosition;
+
+    void Awake()
+    {
+        Assert.IsNotNull(ballPrefab, "ballPrefab is null");
+    }
+
+	void Start ()
+    {
+        ballPosition = mainCamera.GetComponent<Transform>().position + new Vector3(0, -0.5f, 2);
+    }
+	
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            ballObjTmp = GameObject.Instantiate(ballPrefab, ballPosition, Quaternion.identity) as GameObject;
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            ballObjTmp.AddComponent<Rigidbody>();
+
+            Rigidbody rb = ballObjTmp.GetComponent<Rigidbody>();
+            rb.AddForce(force, ForceMode.Force);
+        }
+	}
+}
---
+++ Fighting/Assets/Scripts/Chara.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chara : MonoBehaviour
+{
+    private Animator anim;
+
+    void Start ()
+    {
+        anim = GetComponent<Animator>();
+    }
+	
+	void Update ()
+    {
+	
+	}
+
+    void OnCollisionEnter()
+    {
+        anim.SetBool("DamageDown", true);
+    }
+}
